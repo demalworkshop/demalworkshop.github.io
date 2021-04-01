9f54b3deeb6b9a38b887eb538020ea05d8e252c7
Sumeet Katariya <katsumee@amazon.com>
2021-03-31 23:19:40 -0700

    Added Program

---
+++ js/myjs.js
@@ -0,0 +1,34 @@
+function adjust_times_user_time_zone() {
+    var user_time_zone = moment.tz.guess();
+    $(".workshopitem").each(function (index) {
+        var a = ($(this).data("needed"));
+        var adjusted_time = moment.tz($(this).data("needed"), "America/Los_Angeles").tz(user_time_zone);
+        var text_displacement = '';
+        var day_displacement = moment(adjusted_time.format("YYYY-MM-DD")).diff(moment($(this).data("needed")).format("YYYY-MM-DD"), 'days');
+        if (day_displacement < 0) {
+            text_displacement = ' (' + day_displacement + 'd)';
+        } else if (day_displacement > 0) {
+            text_displacement = ' (+' + day_displacement + 'd)';
+        }
+        $(this).text(adjusted_time.format("HH:mm") + text_displacement);
+    });
+    var formatted_user_time_zone = moment().tz(user_time_zone).format('z');
+    if (formatted_user_time_zone != "CEST" && formatted_user_time_zone != "CET") {
+        $("#time-warning").html('All times are adjusted to <b>your time zone (' + formatted_user_time_zone + ')</b>. Do you want to see times in CEST? <a href="javascript:void(0)" onclick="adjust_times_CEST();">Click here.</a>');
+    } else {
+        $("#time-warning").html('All times are adjusted to <b>your time zone (' + formatted_user_time_zone + ')</b>.');
+    }
+};
+
+function adjust_times_CEST() {
+    $(".workshopitem").each(function (index) {
+        $(this).text(moment.tz($(this).data("needed"), "America/Los_Angeles").tz("Europe/Paris").format("HH:mm"));
+    });
+
+    //$("#time-warning").html('All times are in <b>CEST</b>.');
+    $("#time-warning").html('All times are in <b>CEST</b>. Do you want to see them adjusted to your time zone (' + moment().tz(moment.tz.guess()).format('z') + ')? <a href="javascript:void(0)" onclick="adjust_times_user_time_zone();">Click here.</a>');
+};
+
+$(document).ready(function () {
+    adjust_times_user_time_zone();
+});
